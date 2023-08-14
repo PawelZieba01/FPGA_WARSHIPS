e6c676fa80df3d6b76e2e8a91cec26e53ac2d0cd
PawelZieba01 <zpawel.zieba@gmail.com>
2023-08-14 13:25:51 +0200

    Fix reset/set BRAM warnings

--- rtl/common/delay.v
+++ rtl/common/delay.v
@@ -18,7 +18,7 @@ module delay
 
 //------------------------------------------------------------------------------
 // The first delay stage
-    always @(posedge clk or posedge rst)
+    always @(posedge clk)
     begin:delay_stage_0
         if(rst)
             del_mem[0] <= 0;
@@ -35,7 +35,7 @@ module delay
         for (i = 1; i < CLK_DEL ; i = i + 1 )
         begin:delay_stage
 
-            always @(posedge clk or posedge rst)
+            always @(posedge clk)
             begin
                 if(rst)
                     del_mem[i] <= 0;
